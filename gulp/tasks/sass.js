db041a66cf307e78c1ff3f7e8a010c5965f01d4e
Sebastian Kalicki <sebastian@kalicki.email>
2017-01-13 12:42:15 +0100

    Imported new components in main file.

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -11,13 +11,21 @@ var autoprefixer = require('gulp-autoprefixer'),
 	config = require('../config'),
 	gulp = require('gulp'),
 	sass = require('gulp-sass'),
+	sassdoc = require('sassdoc'),
 	sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('sass', function postcss() {
+gulp.task('sass', () => {
 	return gulp.src(config.sass.src)
 		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer(config.sass.autoprefixer))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest(config.sass.dev));
+});
+
+gulp.task('sass-doc', () => {
+	return gulp.src(config.sass.src)
+		.pipe(sassdoc({
+			dest: 'docs'
+		}));
 });
